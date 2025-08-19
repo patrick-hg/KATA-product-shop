9fea4c52824ab977add969b4c303d949e1518d96
Patrick Hg <patrick.h.hage@gmail.com>
2025-08-19 14:34:02 +0200

    first commit

---
+++ back/src/main/java/com/ph/kata_product_trial/rest/AuthController.java
@@ -0,0 +1,34 @@
+package com.ph.kata_product_trial.rest;
+
+import com.ph.kata_product_trial.auth.AuthenticationDto;
+import com.ph.kata_product_trial.auth.RegistrationDto;
+import com.ph.kata_product_trial.dto.AccountDto;
+import com.ph.kata_product_trial.service.AuthenticationService;
+//import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@Tag(name = "Auth")
+public class AuthController {
+
+    private AuthenticationService authenticationService;
+
+    public AuthController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping(path = "/account")
+    public ResponseEntity register(@RequestBody RegistrationDto registrationDto) {
+        AccountDto accountDto = authenticationService.registerAccount(registrationDto);
+        return ResponseEntity.ok(accountDto);
+    }
+
+    @PostMapping(path = "/token")
+    public ResponseEntity authenticate(@RequestBody AuthenticationDto authenticationDto) {
+        String token = authenticationService.authenticateAccount(authenticationDto);
+        return ResponseEntity.ok(token);
+    }
+}
